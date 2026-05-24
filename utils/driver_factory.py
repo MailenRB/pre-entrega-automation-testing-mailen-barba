6c1f66bfca153d5bc878595f38e8500b41786129
Mailen R.B <mailencaceres8@gmail.com>
2026-05-24 15:21:49 -0300

    feat: agregar factoria del webdriver de Selenium

---
+++ utils/driver_factory.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+def get_chrome_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(10)
+    return driver
